8a240a72a5f1148c1401d3bb024ebb18843d48d0
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-09-14 03:28:17 +0630

    Add social links and gravatar socials data

---
+++ lib/data/socials.ts
@@ -0,0 +1,25 @@
+export const socialLinks = {
+  gravatar: "https://gravatar.com/moekyawaung13721",
+  gravatar2026: "https://gravatar.com/moekyawaung2026",
+  github: "https://github.com/Dev-moe-kyawaung/",
+  linkedin: "https://www.linkedin.com/in/moe-kyaw-aung-2653093a1",
+  youtube: "https://www.youtube.com/channel/UCuTXUguZb4xjeL2nX8WJG",
+  telegram: "https://t.me/moekyawaung",
+  instagram: "https://instagram.com/moekyawaung",
+  playStore: "#",
+  email: "mailto:moekyawaung@programmer.net",
+  phone: "tel:+959889000889",
+  bsky: "https://bsky.app/profile/moekyawaung96.bsky.social",
+  tumblr: "https://www.tumblr.com/moekyawaung",
+  flickr: "https://www.flickr.com/people/204037451@N06",
+  vimeo: "https://vimeo.com/user252414232",
+  slack: "https://moekyawaung.slack.com/",
+  paypal: "https://www.paypal.com/paypalme/my/profile",
+  strikingly: "http://moekyawaung2026.strikingly.com"
+};
+
+export const gravatarSocials = [
+  "GitHub", "LinkedIn", "YouTube", "TikTok", "Reddit", 
+  "Pinterest", "Tumblr", "Flickr", "Twitch", "Vimeo", 
+  "Bluesky", "WordPress"
+];
